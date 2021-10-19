8d0000dd69419d1a3e4bc3778be5870ac9b3961f
Balint Kozari <mrkoba@gmail.com>
2021-10-19 22:27:29 +0200

    feat: create CartController, implement doGet, doPost, doDelete

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,35 +1,61 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.CartDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.service.ProductService;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
+import com.codecool.shop.service.CartService;
+import org.json.JSONException;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
-@WebServlet(urlPatterns = {"/"})
+import org.json.JSONObject;
+
+@WebServlet(urlPatterns = {"/cart"})
 public class CartController extends HttpServlet {
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
+    CartDao cartDao = CartDaoMem.getInstance();
+    ProductDao productDataStore = ProductDaoMem.getInstance();
+    CartService cartService = new CartService(cartDao, productDataStore);
 
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("category", productService.getProductCategory(1));
-        context.setVariable("products", productService.getProductsForCategory(1));
-        engine.process("product/index.html", context, resp.getWriter());
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        String userId = req.getParameter("user-id");
+        List<JSONObject> cartJson = cartService.getCartContent(userId);
+        out.println(cartJson);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        StringBuilder jb = new StringBuilder();
+        String line;
+        BufferedReader reader = req.getReader();
+        while ((line = reader.readLine()) != null)
+            jb.append(line);
+        try {
+            JSONObject payload = new JSONObject(jb.toString());
+            cartService.addToCart(String.valueOf(payload.get("product-id")), String.valueOf(payload.get("user-id")));
+        } catch (JSONException e) {
+            System.out.println("Error parsing JSON request string");
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
+        String productId = req.getParameter("product-id");
+        String userId = req.getParameter("user-id");
+        String deleteType = req.getParameter("type");
+        cartService.handleDelete(userId, productId, deleteType);
+    }
 }
