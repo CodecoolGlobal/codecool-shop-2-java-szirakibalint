671a68596030e2b97e940c1e8630bbafd910807c
Balint Kozari <mrkoba@gmail.com>
2021-10-20 14:46:20 +0200

    feat: add new method to cartDao

--- src/main/java/com/codecool/shop/dao/CartDao.java
+++ src/main/java/com/codecool/shop/dao/CartDao.java
@@ -7,6 +7,7 @@ public interface CartDao {
     void createNewCart(int userId);
     Cart getCartByUserId(int userId);
     Cart getCartById(int cartId);
+    int getProductQuantity(int cartId, Product product);
     void addToCart(int cartId, Product product);
     void removeOneFromCart(int cartId, Product product);
     void removeProductFromCart(int cartId, Product product);
