d4bc8ebe76da42ee864431429fd2ecee3685385a
Balint Kozari <mrkoba@gmail.com>
2021-10-20 19:32:49 +0200

    feat: add new method to CartDao #18

--- src/main/java/com/codecool/shop/dao/CartDao.java
+++ src/main/java/com/codecool/shop/dao/CartDao.java
@@ -3,6 +3,8 @@ package com.codecool.shop.dao;
 import com.codecool.shop.model.Cart;
 import com.codecool.shop.model.Product;
 
+import java.math.BigDecimal;
+
 public interface CartDao {
     void createNewCart(int userId);
     Cart getCartByUserId(int userId);
@@ -12,5 +14,5 @@ public interface CartDao {
     void removeOneFromCart(int cartId, Product product);
     void removeProductFromCart(int cartId, Product product);
     void removeAllFromCart(int cartId);
-
+    BigDecimal getTotalSum(int cartId);
 }
