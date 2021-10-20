b74f45501fa0260da8b9d24aebb7b758e63f9f06
Csilla Lukacs <erzsebet.lk@gmail.com>
2021-10-20 13:23:19 +0200

    feat: add back-end validation to checkout form

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @WebServlet(urlPatterns = "/checkout")
@@ -21,10 +24,34 @@ public class CheckoutController extends HttpServlet {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
-
-        //context.setVariable("categories", productCategoryDataStore.getAll());
-        //context.setVariable("suppliers", supplierDao.getAll());
         engine.process("product/checkout.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        Enumeration<String> parameterNames = req.getParameterNames();
+        Map<String, String> params = new HashMap<>() ;
+
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            String paramValue = req.getParameter(paramName);
+
+            params.put(paramName, paramValue);
+        }
+
+        if (validateFormData(params)) {
+            resp.sendRedirect("/payment");
+        }
+    }
+
+    private boolean validateFormData(Map<String, String> params){
+        for (String value : params.values()){
+            if (value.equals("")) {
+                return false;
+            };
+        }
+        return true;
     }
 
 }
