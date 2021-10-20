ece972bed17688e2a6cb40add32a7c5bb93252b3
Balint Kozari <mrkoba@gmail.com>
2021-10-20 19:35:38 +0200

    feat: create PaymentService #18

--- src/main/java/com/codecool/shop/service/PaymentService.java
+++ src/main/java/com/codecool/shop/service/PaymentService.java
@@ -1,2 +1,24 @@
-package com.codecool.shop.service;public class PaymentService {
+package com.codecool.shop.service;
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.model.Cart;
+
+import java.math.BigDecimal;
+
+public class PaymentService {
+
+    private static final int DEFAULT_CART_ID = 0;
+
+    private final CartDao cartDao;
+
+    public PaymentService(CartDao cartDao) {
+        this.cartDao = cartDao;
+    }
+
+    public BigDecimal getFullPriceForPayment(String cartId) {
+        Cart cart = cartId == null
+                ? cartDao.getCartById(DEFAULT_CART_ID)
+                : cartDao.getCartById(Integer.parseInt(cartId));
+        return cart.getTotalSum();
+    }
 }
