9963261300ed256630d94c41cabf0fadbf04aea4
Csilla Lukacs <erzsebet.lk@gmail.com>
2021-10-21 13:48:45 +0200

    feat: add some sample data

--- src/main/java/com/codecool/shop/service/CartService.java
+++ src/main/java/com/codecool/shop/service/CartService.java
@@ -4,7 +4,6 @@ import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.model.Cart;
 import com.codecool.shop.model.Product;
-import com.google.gson.Gson;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -107,9 +106,4 @@ public class CartService {
         return cartJson;
     }
 
-    public Cart getCartFromJson(String cartJson){
-        Cart cart = new Gson().fromJson(cartJson, Cart.class);
-
-        return cart;
-    }
 }
