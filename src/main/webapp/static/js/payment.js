acede875c425886a7f68a01e1d1f40fe5b413a18
szirakibalint <blntsziraki@gmail.com>
2021-10-21 11:20:55 +0200

    refactor: simplify Credit Card form fields #23

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -14,23 +14,17 @@ function getPayPalForm() {
 }
 
 function getCreditCardFrom() {
-    return `<label for="payment-form">Enter Credit Card details:</label>
-             <form id="payment-form">
-              <div class="form-group">
-                <label for="card-number">Card number:</label>
-                <input type="text" class="form-control" id="card-number" placeholder="card number">
+    return `<form id="payment-form">
+              <label for="card-details">Enter Credit Card details:</label>
+              <div class="form-group" id="card-details">
+                <input type="text" class="form-control" id="card-number" placeholder="1234 1234 1234 1234">
+                <input type="number" class="form-control" id="expiration-month" placeholder="MM">
+                <input type="number" class="form-control" id="expiration-year" placeholder="YY">
+                <input type="text" class="form-control" id="cvv" placeholder="123">
               </div>
               <div class="form-group">
-                <label for="card-holder">Card holder:</label>
-                <input type="text" class="form-control" id="card-holder" placeholder="card holder">
-              </div>
-              <div class="form-group">
-                <label for="expiration">Expiration:</label>
-                <input type="text" class="form-control" id="expiration" placeholder="expiration">
-              </div>
-              <div class="form-group">
-                <label for="cvv">CVV:</label>
-                <input type="text" minlength="3" maxlength="3" class="form-control" id="cvv" placeholder="CVV">
+                <label for="card-holder">Name on card:</label>
+                <input type="text" class="form-control" id="card-owner" placeholder="name on card">
               </div>
               <button type="submit" class="btn btn-primary">Pay with Credit Card</button>
             </form>`
