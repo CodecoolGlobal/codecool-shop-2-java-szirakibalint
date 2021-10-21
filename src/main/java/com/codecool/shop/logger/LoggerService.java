c8bea9a72845bf5fdb10562c679d1787c0edc103
Balint Kozari <mrkoba@gmail.com>
2021-10-21 10:10:21 +0200

    feat: create LoggerService #27

---
+++ src/main/java/com/codecool/shop/logger/LoggerService.java
@@ -0,0 +1,21 @@
+package com.codecool.shop.logger;
+
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.model.Order;
+import org.json.JSONObject;
+
+public class LoggerService {
+
+    private final OrderDao orderDao;
+
+    public LoggerService(OrderDao orderDao) {
+        this.orderDao = orderDao;
+    }
+
+    public void logOrder(int orderId) {
+        Order order = orderDao.find(orderId);
+        JSONObject jsonObject = new JSONObject(){{
+            order.get
+        }};
+    }
+}
