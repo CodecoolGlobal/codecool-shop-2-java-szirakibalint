c45807aaa3ac72be73f7a01a54154b0919e1607a
Balint Kozari <mrkoba@gmail.com>
2021-10-21 22:30:55 +0200

    feat: make Order abstract #27

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -1,97 +1,41 @@
 package com.codecool.shop.model;
 
+import org.json.JSONObject;
+
+import javax.management.remote.JMXServerErrorException;
 import java.time.LocalDateTime;
 import java.util.Map;
 
-public class Order {
+public abstract class Order {
     private static int idCounter = 0;
 
-    private final int id;
-    private final String firstName;
-    private final String lastName;
-    private final String country;
-    private final String city;
-    private final String address;
-    private LocalDateTime orderedAt;
+    protected final int id;
+    protected final LocalDateTime orderedAt;
     private boolean paidFor = false;
-    private final int userId;
-    private final Cart cart;
-    private Map<Product, Integer> products;
+    protected final Cart cart;
 
-    public Order(String firstName, String lastName, String country, String city, String address, int userId, Cart cart){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.country = country;
-        this.city = city;
-        this.address = address;
+    public Order(Cart cart) {
         this.id = idCounter;
         idCounter++;
-
-        this.userId = userId;
-        this.cart = cart;
-        products = cart.getProducts();
         this.orderedAt = LocalDateTime.now();
-
-    }
-
-    public Cart getCart() {
-        return cart;
+        this.cart = cart;
     }
 
     public int getId() {
         return id;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
     public LocalDateTime getOrderedAt() {
         return orderedAt;
     }
 
-    public boolean isPaidFor() {
-        return paidFor;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public Map<Product, Integer> getProducts() {
-        return products;
-    }
-
     public void pay(){
-        if (!paidFor) {
+        if (!paidFor && this instanceof ValidOrder) {
             this.paidFor = true;
         } else {
             throw new RuntimeException("already paid for this!!");
         }
     }
 
-    @Override
-    public String toString() {
-        return orderedAt +
-                " " + firstName +
-                " " + lastName +
-                " " + address +
-                " " + products.toString();
-    }
+    public abstract JSONObject toJson();
 }
