ce49f58557730417a8b1baf577b413f322ae3f4c
szirakibalint <blntsziraki@gmail.com>
2021-10-21 11:22:27 +0200

    feat: require input on Credit Card form #23

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -17,14 +17,14 @@ function getCreditCardFrom() {
     return `<form id="payment-form">
               <label for="card-details">Enter Credit Card details:</label>
               <div class="form-group" id="card-details">
-                <input type="text" class="form-control" id="card-number" placeholder="1234 1234 1234 1234">
-                <input type="number" class="form-control" id="expiration-month" placeholder="MM">
-                <input type="number" class="form-control" id="expiration-year" placeholder="YY">
-                <input type="text" class="form-control" id="cvv" placeholder="123">
+                <input required type="text" class="form-control" id="card-number" placeholder="1234 1234 1234 1234">
+                <input required type="number" class="form-control" id="expiration-month" placeholder="MM">
+                <input required type="number" class="form-control" id="expiration-year" placeholder="YY">
+                <input required type="text" class="form-control" id="cvv" placeholder="123">
               </div>
               <div class="form-group">
                 <label for="card-holder">Name on card:</label>
-                <input type="text" class="form-control" id="card-owner" placeholder="name on card">
+                <input required type="text" class="form-control" id="card-owner" placeholder="name on card">
               </div>
               <button type="submit" class="btn btn-primary">Pay with Credit Card</button>
             </form>`
