90fd92417aa63bef8a4174dee91be523f1faddc4
Balint Kozari <mrkoba@gmail.com>
2021-11-02 19:33:14 +0100

    feat: implement add and find methods #10

--- src/main/java/com/codecool/shop/dao/jdbc/SupplierDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/jdbc/SupplierDaoJDBC.java
@@ -4,6 +4,7 @@ import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Supplier;
 
 import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 
 public class SupplierDaoJDBC implements SupplierDao{
@@ -24,12 +25,33 @@ public class SupplierDaoJDBC implements SupplierDao{
 
     @Override
     public void add(Supplier supplier) {
-        
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "INSERT INTO supplier (name, description) VALUES (?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, supplier.getName());
+            statement.setString(2, supplier.getDescription());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Supplier find(int id) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, name, description FROM supplier WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Supplier supplier = new Supplier(resultSet.getString("name"), resultSet.getString("description"));
+                supplier.setId(resultSet.getInt("id"));
+                return supplier;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
