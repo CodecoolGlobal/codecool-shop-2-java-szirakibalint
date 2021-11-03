4af90f35762047ada7c30ee6ca950e44a4ad75a1
Balint Kozari <mrkoba@gmail.com>
2021-11-03 14:50:05 +0100

    feat: create cartmapper to handle database related tasks #10

--- src/main/java/com/codecool/shop/mapper/CartMapper.java
+++ src/main/java/com/codecool/shop/mapper/CartMapper.java
@@ -1,2 +1,25 @@
-package com.codecool.shop.mapper;public class CartMapper {
+package com.codecool.shop.mapper;
+
+import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.Product;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CartMapper {
+
+    private final ProductMapper productMapper = new ProductMapper();
+
+    public Cart createCartFromResultSet(ResultSet resultSet) throws SQLException {
+        Cart cart = new Cart();
+        cart.setId(resultSet.getInt("cart_id"));
+        cart.setUserId(resultSet.getInt("user_id"));
+        Product product = productMapper.createProductFromResultSet(resultSet);
+        cart.add(product);
+        while(resultSet.next()) {
+            product = productMapper.createProductFromResultSet(resultSet);
+            cart.add(product);
+        }
+        return cart;
+    }
 }
